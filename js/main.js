676dfbbe87d08a774df6ea55683d4362c606ccbd
fulprimitiv <eremeevdima51@gmail.com>
2023-12-22 23:47:53 +0500

    12.12. Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -3,11 +3,22 @@ import {openUserModalElement} from './full-size-image.js';
 import {openImageUpload} from './form.js';
 import {getData} from './api.js';
 import {showAlert} from './util.js';
+import {clickDefaultFilter, clickRandomFilter, clickDiscussedFilter} from './filters.js';
+import {debounce} from './util.js';
 
 getData()
   .then((photoArray) => {
     renderThumbnails(photoArray);
     openUserModalElement(photoArray);
+    clickDefaultFilter(debounce(
+      () => renderThumbnails(photoArray)
+    ));
+    clickRandomFilter(debounce(
+      () => renderThumbnails(photoArray, 'random')
+    ));
+    clickDiscussedFilter(debounce(
+      () => renderThumbnails(photoArray, 'discussed')
+    ));
   })
   .catch((err) => showAlert(err.message));
 
